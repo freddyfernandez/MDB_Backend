e0b71cddfef80351af4d81dd3600168e0a1e4692
freddy fernandez <fernandezyc@hotmail.com>
2021-03-14 16:17:03 -0500

    SPA HOME V01.1

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -92,3 +92,19 @@ function mostrar(x) {
 
     }
 }
+
+window.fbAsyncInit = function() {
+    FB.init({
+        xfbml: true,
+        version: 'v10.0'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
